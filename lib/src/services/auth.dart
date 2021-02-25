49fe09aeaf62d136341064bca9ec584d180424b3
Habibatarek22 <64978340+Habibatarek22@users.noreply.github.com>
2021-02-26 00:06:25 +0200

    inshallah e-load

---
+++ lib/src/services/auth.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:medicine_reminder/src/models/medicalinfo.dart';
+
+class Auth {
+  final _auth = FirebaseAuth.instance;
+
+  Future<AuthResult> signUp(String email, String password) async {
+    final authResult = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return authResult;
+  }
+}
