0039bc77d6678c47428336f75b7c70517d8aa985
esraa-hassan8097 <61292920+esraa-hassan8097@users.noreply.github.com>
2021-03-06 23:36:12 +0200

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -47,180 +47,176 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        resizeToAvoidBottomPadding: false,
+        //resizeToAvoidBottomPadding: false,
         body: ListView(children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                      child: Text('PLEASE',
-                          style: TextStyle(
-                              fontSize: 80.0, fontWeight: FontWeight.bold)),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10.0, 185.0, 0.0, 0.0),
-                      child: Text('SIGNIN',
-                          style: TextStyle(
-                              fontSize: 80.0, fontWeight: FontWeight.bold)),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(270.0, 185.0, 0.0, 0.0),
-                      child: Text('.',
-                          style: TextStyle(
-                              fontSize: 80.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.purple)),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                  child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return "Please enter email here";
-                              }
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                                labelText: 'EMAIL',
-                                labelStyle: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.grey),
-                                focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.purple))),
-                          ),
-                          SizedBox(height: 20.0),
-                          TextFormField(
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return "Please enter email here";
-                              }
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                                labelText: 'PASSWORD',
-                                labelStyle: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.grey),
-                                focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.purple))),
-                            obscureText: true,
-                          ),
-                          SizedBox(height: 5.0),
-                          Container(
-                            alignment: Alignment(1.0, 0.0),
-                            padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                            child: InkWell(
-                              child: Text(
-                                'Forgot Password?',
-                                style: TextStyle(
-                                    color: Colors.purple,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat',
-                                    decoration: TextDecoration.underline),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 40.0),
-                          Container(
-                            height: 40.0,
-                            child: Material(
-                              borderRadius: BorderRadius.circular(20.0),
-                              shadowColor: Colors.purpleAccent,
-                              color: Colors.purple,
-                              elevation: 7.0,
-                              child: GestureDetector(
-                                onTap: () {
-                                  if (_formKey.currentState.validate()) {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => HomePage()));
-                                  }
-                                },
-                                child: Center(
-                                  child: Text(
-                                    'LOGIN',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Montserrat'),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10.0),
-                          Container(
-                            height: 40.0,
-                            child: Material(
-                              borderRadius: BorderRadius.circular(20.0),
-                              shadowColor: Colors.purpleAccent,
-                              color: Colors.purple,
-                              elevation: 7.0,
-                              child: GestureDetector(
-                                onTap: () {
-                                  //if (_formKey.currentState.validate()) {
-                                  Navigator.of(context).pushNamed('/signup');
-                                  // }
-                                },
-                                child: Center(
-                                  child: Text(
-                                    'New to the app? Register',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Montserrat'),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ))),
-              SizedBox(height: 17.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Are you an admin ?',
-                    style: TextStyle(fontFamily: 'Montserrat'),
-                  ),
-                  SizedBox(width: 5.0),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Adminlogin()));
-                    },
-                    child: Text(
-                      'sign in as admin',
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text('PLEASE',
                       style: TextStyle(
-                          color: Colors.purple,
-                          fontFamily: 'Montserrat',
+                          fontSize: 80.0, fontWeight: FontWeight.bold)),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10.0, 185.0, 0.0, 0.0),
+                  child: Text('SIGNIN',
+                      style: TextStyle(
+                          fontSize: 80.0, fontWeight: FontWeight.bold)),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(270.0, 185.0, 0.0, 0.0),
+                  child: Text('.',
+                      style: TextStyle(
+                          fontSize: 80.0,
                           fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline),
-                    ),
-                  )
-                ],
+                          color: Colors.purple)),
+                )
+              ],
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+              child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Please enter email here";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'EMAIL',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.purple))),
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Please enter email here";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'PASSWORD',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.purple))),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 5.0),
+                      Container(
+                        alignment: Alignment(1.0, 0.0),
+                        padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                        child: InkWell(
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Montserrat',
+                                decoration: TextDecoration.underline),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40.0),
+                      Container(
+                        height: 40.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.purpleAccent,
+                          color: Colors.purple,
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {
+                              if (_formKey.currentState.validate()) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomePage()));
+                              }
+                            },
+                            child: Center(
+                              child: Text(
+                                'LOGIN',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      Container(
+                        height: 40.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.purpleAccent,
+                          color: Colors.purple,
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {
+                              //if (_formKey.currentState.validate()) {
+                              Navigator.of(context).pushNamed('/signup');
+                              // }
+                            },
+                            child: Center(
+                              child: Text(
+                                'New to the app? Register',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))),
+          SizedBox(height: 17.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Are you an admin ?',
+                style: TextStyle(fontFamily: 'Montserrat'),
+              ),
+              SizedBox(width: 5.0),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Adminlogin()));
+                },
+                child: Text(
+                  'sign in as admin',
+                  style: TextStyle(
+                      color: Colors.purple,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline),
+                ),
               )
             ],
           )
-        ]));
+        ],
+      )
+    ]));
   }
 }
